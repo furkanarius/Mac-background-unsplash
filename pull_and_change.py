0a55cefd25bd4adfbd46e322cc334e23fbe39263
Furkan Ak <furkan.akyurek@trtworld.com>
2017-09-01 00:32:12 +0300

    Update pull_and_change.py

--- pull_and_change.py
+++ pull_and_change.py
@@ -7,11 +7,10 @@ import random
 def main():
     try:
         page = requests.get(
-            'https://api.unsplash.com/photos/random?client_id=aaaf91ab8754ae02756508328bdf10a5f9d502405683f853c6ce302e04f98d4f&orientation=landscape')
+            'https://api.unsplash.com/photos/random?client_id=CLIENT_ID_COMES_HERE&orientation=landscape')
         content = json.loads(page.content)
         url = content["urls"]["raw"]
-        out_filepath = '/Users/furkan/Pictures/Selection/Unsplash/' + str(random.randint(100, 100000000)) + ".jpg"
-        # 'sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = \"/Users/Furkan/Pictures/Selection/night-1.jpg\"";'
+        out_filepath = '/Users/USERNAME/Pictures/Selection/Unsplash/' + str(random.randint(100, 100000000)) + ".jpg"
         filename = wget.download(url, out=out_filepath)
         return filename
     except:
